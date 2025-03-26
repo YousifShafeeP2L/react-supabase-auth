81b2c63cc14b551b8c541fce93f1aa0a1905652c
YousifShafeeP2L <yousif@path2live.com>
2025-03-26 11:53:44 +0200

    remove Dashboard component and integrate user authentication in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import supabase from "../utils/supabaseClient";
 
 function Home() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Get current user data when component mounts
+    const getCurrentUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    getCurrentUser();
+  }, []);
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+    navigate("/login");
+  };
+
   return (
     <div>
-      <Link to="/register">Register</Link>
-      <br></br>
-      <Link to="/login">Login</Link>
+      <h1>Hello {user?.email}</h1>
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 }
