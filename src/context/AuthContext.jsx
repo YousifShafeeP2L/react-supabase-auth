092625ed984008e5b935a7abdb05713e2b4a089a
YousifShafeeP2L <yousif@path2live.com>
2025-03-26 11:52:31 +0200

    refactor the auth wrapper to context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useState } from 'react';
+import supabase from '../utils/supabaseClient';
+
+export const AuthContext = createContext({ user: null, loading: true });
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setUser(session?.user ?? null);
+      setLoading(false);
+    };
+
+    getSession();
+
+    // Optionally, listen to changes in auth state
+    const { subscription } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+      }
+    );
+
+    return () => {
+      subscription?.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
